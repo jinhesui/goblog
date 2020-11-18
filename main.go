2e81acf90148bed11a3e8c952cc4c67da280eb1f
jinhesui <jinhesui@gmail.com>
2020-11-18 07:47:44 +0000

    自动重载

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>临兵斗者，皆阵列前行</h1>")
+		fmt.Fprint(w, "<h1>临兵斗者，皆阵列前行。前方高能预警</h1>")
 	} else if r.URL.Path == "/about" {
 		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系"+"<a href=\"mailto:jinhesui@163.com\">jinhesui@163.com</a>")
 	} else {
