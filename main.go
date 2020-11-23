316362f3e58e50f0bf3d701212287ce1702425f9
jinhesui <jinhesui@gmail.com>
2020-11-23 08:04:24 +0000

    读取请求数据

--- main.go
+++ main.go
@@ -34,7 +34,10 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "创建新的文章")
+	fmt.Fprintf(w, "r.Form 中 title 的值为：%v <br>", r.FormValue("title"))
+	fmt.Fprintf(w, "r.PostForm 中 title 的值为：%v <br>", r.PostFormValue("title"))
+	fmt.Fprintf(w, "r.Form 中 test 的值为： %v <br>", r.FormValue("test"))
+	fmt.Fprintf(w, "r.PostForm 中 test 的值为：%v <br>", r.PostFormValue("test"))
 }
 
 func forceHTMLMiddleware(next http.Handler) http.Handler {
@@ -59,23 +62,22 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 
 func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
 	html := `
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<title>创建文章 — 我的技术博客</title>
-	</head>
-	<body>
-		<form action="%s" method="post">
-			<p><input type="text" name="title"></p>
-			<p><textarea name="body" cols="30" rows="10"></textarea></p>
-			<p><button type="submit">提交</button></p>
-		</form>
-	</body>
-	</html>
-	`
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>创建文章 — 我的技术博客</title>
+</head>
+<body>
+	<form action="%s?test=data" method="post">
+		<p><input type="text" name="title"></p>
+		<p><textarea name="body" cols="30" rows="10"></textarea></p>
+		<p><button type="submit">提交</button></p>
+	</form>
+</body>
+</html>
+`
 	storeURL, _ := router.Get("articles.store").URL()
-	fmt.Fprint(w, html, storeURL)
+	fmt.Fprintf(w, html, storeURL)
 }
 
 func main() {
@@ -92,12 +94,6 @@ func main() {
 
 	// 中间件：强制内容为 HTML
 	router.Use(forceHTMLMiddleware)
-
-	// 通过命名路由获取 URL 示例
-	// homeURL, _ := router.Get("home").URL()
-	// fmt.Println("homeURL: ", homeURL)
-	// articleURL, _ := router.Get("articles.show").URL("id", "1")
-	// fmt.Println("articleURL: ", articleURL)
 
 	http.ListenAndServe(":3000", removeTrailingSlash(router))
 }
