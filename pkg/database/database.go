82447d719ff115c52c5684f2d0f3a737ac450d82
jinhesui <jinhesui@gmail.com>
2020-11-26 10:05:30 +0000

    重构至 PagesController

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -21,24 +21,24 @@ func initDB() {
 	var err error
 	// 设置数据库连接信息
 	config := mysql.Config{
-		User:   				"root",
-		Passwd: 				"secret",
-		Addr:   				"127.0.0.1:3306",
-		Net:					"tcp",
-		DBName:					"goblog",
-		AllowNativePasswords:	true,
+		User:                 "homestead",
+		Passwd:               "secret",
+		Addr:                 "127.0.0.1:33060",
+		Net:                  "tcp",
+		DBName:               "goblog",
+		AllowNativePasswords: true,
 	}
 	// 准备数据库连接池
-	db, err = sql.Open("mysql", config.FormatDSN())
+	DB, err = sql.Open("mysql", config.FormatDSN())
 	logger.LogError(err)
 	// 设置最大连接数
-	db.SetMaxOpenConns(100)
+	DB.SetMaxOpenConns(100)
 	// 设置最大空闲连接数
-	db.SetMaxIdleConns(25)
+	DB.SetMaxIdleConns(25)
 	// 设置每个链接的过期时间
-	db.SetConnMaxLifetime(5 * time.Minute)
+	DB.SetConnMaxLifetime(5 * time.Minute)
 	// 尝试连接，失败会报错
-	err = db.Ping()
+	err = DB.Ping()
 	logger.LogError(err)
 }
 
@@ -48,6 +48,6 @@ id bigint(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,
 title varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
 body longtext COLLATE utf8mb4_unicode_ci
 	);`
-	_, err := db.Exec(createArticlesSQL)
+	_, err := DB.Exec(createArticlesSQL)
 	logger.LogError(err)
 }
