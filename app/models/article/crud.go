ae8ad0b923cc3ae7f52d3b3b137892a4844d0072
jinhesui <jinhesui@gmail.com>
2020-11-27 10:02:15 +0000

    重构创建文章

--- app/models/article/crud.go
+++ app/models/article/crud.go
@@ -1,6 +1,7 @@
 package article
 
 import (
+	"goblog/pkg/logger"
 	"goblog/pkg/model"
 	"goblog/pkg/types"
 )
@@ -22,4 +23,13 @@ func GetAll() ([]Article, error) {
 		return articles, err
 	}
 	return articles, nil
+}
+
+// Create 创建文章，通过 article.ID 来判断是否创建成功
+func (article *Article) Create() (err error) {
+	result := model.DB.Create(&article)
+	if err = result.Error; err != nil {
+		logger.LogError(err)
+	}
+	return nil
 }
