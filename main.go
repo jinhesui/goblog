48c175f4ee83b608b01e2651f5c98d9c82c74934
jinhesui <jinhesui@gmail.com>
2020-11-30 03:10:30 +0000

    重构中间件

--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
-	"goblog/pkg/database"
 	"net/http"
-	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -33,39 +32,14 @@ type Article struct {
 	ID          int64
 }
 
-func forceHTMLMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1.设置标头
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		// 2.继续处理请求
-		next.ServeHTTP(w, r)
-	})
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1.除首页以外，移除所有请求路径后面的斜杠
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-		// 2.将请求传递下去
-		next.ServeHTTP(w, r)
-	})
-}
-
 func getRouteVariable(parameterName string, r *http.Request) string {
 	vars := mux.Vars(r)
 	return vars[parameterName]
 }
 
 func main() {
-	database.Initialize()
-	db = database.DB
 	bootstrap.SetupDB()
-	router = bootstrap.SetupRoute()
+	router := bootstrap.SetupRoute()
 
-	// 中间件：强制内容为 HTML
-	router.Use(forceHTMLMiddleware)
-
-	http.ListenAndServe(":3000", removeTrailingSlash(router))
+	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
 }
